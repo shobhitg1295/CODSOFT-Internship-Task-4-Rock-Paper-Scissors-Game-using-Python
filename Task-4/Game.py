46383fbb2a6b7d804e0dd0a73604780564e5534d
shobhitg1295 <shobhitg1295@gmail.com>
2025-08-27 22:35:01 +0530

    Update Game.py

--- Task-4/Game.py
+++ Task-4/Game.py
@@ -1,1 +1,38 @@
+import random
 
+print("Welcome to Rock-Paper-Scissors Game!")
+
+while True:
+    user = input("Enter Rock, Paper, or Scissors: ").lower()
+
+    choices = ["rock", "paper", "scissors"]
+    computer = random.choice(choices)
+
+    print("You chose:", user)
+    print("Computer chose:", computer)
+
+    if user == computer:
+        print("It's a Tie!")
+    elif user == "rock":
+        if computer == "scissors":
+            print("You Win! Rock beats Scissors.")
+        else:
+            print("You Lose! Paper beats Rock.")
+    elif user == "paper":
+        if computer == "rock":
+            print("You Win! Paper beats Rock.")
+        else:
+            print("You Lose! Scissors beat Paper.")
+    elif user == "scissors":
+        if computer == "paper":
+            print("You Win! Scissors beat Paper.")
+        else:
+            print("You Lose! Rock beats Scissors.")
+    else:
+        print("Invalid choice! Please type Rock, Paper, or Scissors.")
+
+    play_again = input("Do you want to play again? (yes/no): ").lower()
+    if play_again != "yes":
+        print("Thanks for playing! Goodbye 👋")
+        break
+      
